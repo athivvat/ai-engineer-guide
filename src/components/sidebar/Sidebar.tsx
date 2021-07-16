eefdeb8a4ce397b23394666e66081379e5d81a8a
athivvat <athivvat@gmail.com>
2021-07-16 16:14:25 +0700

    Mobile menu

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,14 @@
-import { Ads } from "../ads"
+import clsx from "clsx";
+import { Ads } from "../ads";
 
-const Sidebar = () => {
+const Sidebar = ({ show }) => {
   return (
-    <div className="flex flex-col justify-between bg-blue-700 text-blue-50 w-64 p-4">
+    <div
+      className={clsx(
+        "flex flex-col justify-between bg-blue-700 text-white w-64 p-4 absolute md:relative md:translate-x-0 inset-y-0 left-0 transform transition duration-200 ease-in-out",
+        !show && "-translate-x-full"
+      )}
+    >
       <div>
         <a
           href="/"
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
 import { Header } from "../components/header";
 import { Sidebar } from "../components/sidebar";
 
 const Layout = ({ children }) => {
+  const [show, setShow] = useState(false);
   return (
     <>
-      <div className="relative min-h-screen flex">
-        <Sidebar />
+      <div className="relative min-h-screen md:flex">
+        <div className="flex justify-between bg-gray-800 text-gray-100 md:hidden">
+          <a
+            href="/"
+            className="font-medium p-4 tracking-wider transition-colors hover:text-white uppercase focus:text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-sky-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none rounded"
+          >
+            AI Engineer Roadmap
+          </a>
+          <button
+            className="p-4 focus:outline-none focus:bg-gray-700"
+            onClick={() => setShow(!show)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+        <Sidebar show={show} />
         <div className="flex-1">
           <Header />
-          <div className="p-10">{children}</div>
+          <div className="p-4 xl:p-10">{children}</div>
         </div>
       </div>
     </>
